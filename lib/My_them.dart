2519d1133de640a8431755840fd53021bfc24ae3
ON LINE <nadagamal2622@gmail.com>
2023-09-13 02:45:58 +0300

    finish home screen.

---
+++ lib/My_them.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  static Color primaryLight = Color(0xffB7935F);
+  static Color blackColor = Color(0xff242424);
+  static Color whiteColor = Color(0xffF8F8F8);
+  static ThemeData lightMode = ThemeData(
+      primaryColor: primaryLight,
+
+      /// canvasColor:primaryLight,    ///no use
+      scaffoldBackgroundColor: Colors.transparent,
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        selectedItemColor: blackColor,
+        unselectedItemColor: whiteColor,
+
+        ///showUnselectedLabels:true,
+      ),
+      textTheme: TextTheme(
+          titleLarge: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 30, color: Colors.black)));
+}
